2be8365d476f2803b8bb5137719b9eabdcad96c4
Edgardo Morales <edgardo.morales@gigigo.com>
2025-02-28 12:36:43 -0600

    [ADD] Leer el archivo JSON

---
+++ miniapi/routes/clase2.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+/* GET home page. */
+router.get('/', function (req, res) {
+  const pathFile = path.join(__dirname, '../data/movies.json')
+  const file = fs.readFileSync(pathFile, 'utf-8')
+  const movies = JSON.parse(file)
+  res.render('index',movies[1])
+});
+
+
+
+module.exports = router;
