f8b21504193a636607bcd513bc8e0a07ac51da6a
Edgardo Morales <edgardo.morales@gigigo.com>
2025-02-28 09:56:16 -0600

    using views

--- miniapi/routes/index.js
+++ miniapi/routes/index.js
@@ -6,7 +6,20 @@ const path = require('path');
 router.get('/', function (req, res) {
   const pathFile = path.join(__dirname, '../data/movies.csv')
   const file = fs.readFileSync(pathFile, 'utf-8')
-  res.send(file)
+  const lines = file.split('\n')
+  const movies = []
+  const headers = lines[0].split(',')
+  for (let i = 1; i < lines.length; i++) {
+    const movie = {}
+    const currentLine = lines[i].split(',')
+    for (let j = 0; j < headers.length; j++) {
+      movie[headers[j]] = currentLine[j]
+    }
+    movies.push(movie)
+  }
+  res.render('index',movies[1])
 });
+
+
 
 module.exports = router;
